6bcbb4a68955f37112b03414c169d9183d755ba5
Amber <>
2020-10-24 21:05:21 -0500

    First Commit

---
+++ src/main/java/com/meritamerica/assignment4/BankAccount.java
@@ -0,0 +1,86 @@
+package com.meritamerica.assignment4;
+
+import java.util.*;
+import java.text.*;
+
+public abstract class BankAccount {
+
+	double balance;
+	double interestRate;
+	long accountNumber;
+	Date accountOpenedOn;
+	static SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+	ArrayList<Transaction> arrTran = new ArrayList<Transaction>();
+
+	public BankAccount(double balance, double interestRate) {
+		this.balance = balance;
+		this.interestRate = interestRate;
+	}
+
+	public BankAccount(double balance, double interestRate, java.util.Date accountOpenedOn) {
+		this.balance = balance;
+		this.interestRate = interestRate;
+		this.accountOpenedOn = accountOpenedOn;
+
+	}
+
+	public BankAccount(long accountNumber, double balance, double interestRate, java.util.Date accountOpenedOn) {
+		this.accountNumber = accountNumber;
+		this.balance = balance;
+		this.interestRate = interestRate;
+		this.accountOpenedOn = accountOpenedOn;
+	}
+
+	java.util.Date getOpenedOn() {
+		return accountOpenedOn;
+	}
+
+	long getAccountNumber() {
+		return accountNumber;
+	}
+
+	double getBalance() {
+		return balance;
+	}
+
+	double getInterestRate() {
+		return interestRate;
+	}
+
+	public boolean withdraw(double amount) {
+		if (amount < 0 || amount > balance) {
+			return false;
+		} else {
+			balance -= amount;
+		}
+		return true;
+	}
+
+	boolean deposit(double amount) {
+		if (amount < 0) {
+			return false;
+		} else {
+			balance += amount;
+		}
+		return true;
+	}
+
+	double futureValue(int years) {
+		double recInterest = interestRate + 1;
+		double recAmmount;
+		for (int i = 0; (i < years-1); i++) {
+			recInterest = recInterest * (interestRate + 1);
+		}
+		recAmmount = recInterest * balance;
+		return recAmmount;
+
+	}
+
+	public void addTransaction(Transaction transaction) {
+		arrTran.add(transaction);
+	}
+
+	public List<Transaction> getTransaction() {
+		return arrTran;
+	}
+}
---
+++ src/main/java/com/meritamerica/assignment4/CDAccount.java
@@ -0,0 +1,73 @@
+package com.meritamerica.assignment4;
+
+import java.text.*;
+import java.util.*;
+
+import com.meritamerica.assignment4.CDOffering;
+import com.meritamerica.assignment4.MeritBank;
+
+public class CDAccount extends BankAccount {
+
+	CDOffering offerings;
+	int term;
+
+	public CDAccount(CDOffering offerings, double balance) {
+		super(balance, offerings.interestRate);
+		this.offerings = offerings;
+
+	}
+
+	public CDAccount(long accountNumber, double balance, double interestRate, java.util.Date dateOpenedOn, int term) {
+		super(accountNumber, balance, interestRate, dateOpenedOn);
+		this.term = term;
+
+	}
+
+	public boolean withdraw(double amount) {
+		return false;
+	}
+
+	public boolean deposit(double amount) {
+		return false;
+	}
+
+	public long getAccountNumber() {
+		return accountNumber;
+
+	}
+
+	int getTerm() {
+		return term;
+	}
+
+	public double futureValue() throws NegativeAmountException, ExceedsFraudSuspicionLimitException {
+		
+		double recInterest = offerings.getInterestRate() + 1;
+		double recAmmount;
+		for (int i = 0; i < (offerings.getTerm()-1); i++) {
+			recInterest = recInterest * (offerings.getInterestRate() + 1);
+		}
+		
+
+		recAmmount = recInterest * balance;
+		return recAmmount;
+	}
+
+	static CDAccount readFromString(String accountData) throws ParseException {
+		CDAccount cda;
+
+		try {
+			ArrayList<String> x = new ArrayList<>(Arrays.asList(accountData.split(",")));
+			long acNum = Long.parseLong(x.get(0));
+			double b = Double.parseDouble(x.get(1));
+			double ir = Double.parseDouble(x.get(2));
+			Date date = formatter.parse(x.get(3));
+			int term = Integer.parseInt(x.get(4));
+			cda = new CDAccount(acNum, b, ir, date, term);
+		} catch (ParseException ex) {
+			throw new java.lang.NumberFormatException();
+		}
+		return cda;
+	}
+
+}
